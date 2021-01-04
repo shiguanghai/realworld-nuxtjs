7b17accac663dca497274bf41bd8b8f9af050a1c
shiguanghai <1932275668@qq.com>
2021-01-04 23:21:02 +0800

    项目初始化

---
+++ nuxt.config.js
@@ -0,0 +1,60 @@
+/**
+ * Nuxt.js 配置文件
+ */
+
+module.exports = {
+  router: {
+    linkActiveClass: 'active',
+    // 自定义路由表规则
+    // routes: 一个数组，路由配置表
+    // resolve: 解析路由组件路径
+    extendRoutes(routes, resolve) {
+      // 清除 Nuxt.js 基于 pages 目录默认生成的路由表规则
+      routes.splice(0)
+      // 添加自己的路由规则
+      routes.push(...[
+        {
+          path: '/',
+          component: resolve(__dirname, 'pages/layout/'),
+          children: [
+            {
+              path: '', // 默认子路由
+              name: 'home',
+              component: resolve(__dirname, 'pages/home/')
+            },
+            {
+              path: '/login',
+              name: 'login',
+              component: resolve(__dirname, 'pages/login/')
+            },
+            {
+              path: '/register',
+              name: 'register',
+              component: resolve(__dirname, 'pages/login/')
+            },
+            {
+              path: '/profile/:username',
+              name: 'profile',
+              component: resolve(__dirname, 'pages/profile/')
+            },
+            {
+              path: '/settings',
+              name: 'settings',
+              component: resolve(__dirname, 'pages/settings/')
+            },
+            {
+              path: '/editor',
+              name: 'editor',
+              component: resolve(__dirname, 'pages/editor/')
+            },
+            {
+              path: '/article/:slug',
+              name: 'article',
+              component: resolve(__dirname, 'pages/article/')
+            }
+          ]
+        }
+      ])
+    }    
+  }
+}
---
+++ pages/layout/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <!-- 顶部导航栏 -->
+    <nav class="navbar navbar-light">
+      <div class="container">
+        <!-- <a class="navbar-brand" href="index.html">conduit</a> -->
+        <nuxt-link class="navbar-brand" to="/">conduit</nuxt-link>
+        <ul class="nav navbar-nav pull-xs-right">
+          <li class="nav-item">
+            <!-- Add "active" class when you're on that page" -->
+            <!-- <a class="nav-link active" href="">Home</a> -->
+            <!-- 精确匹配，只有路由等于'/'才会高亮 -->
+            <nuxt-link class="nav-link" to="/" exact>Home</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href=""> -->
+            <nuxt-link class="nav-link" to="/editor">
+              <i class="ion-compose"></i>&nbsp;New Post
+            </nuxt-link>  
+            <!-- </a> -->
+          </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href=""> -->
+            <nuxt-link class="nav-link" to="/settings">  
+              <i class="ion-gear-a"></i>&nbsp;Settings
+            </nuxt-link>   
+            <!-- </a> -->
+          </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="">Sign up</a> -->
+            <nuxt-link class="nav-link" to="/register">Sign up</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/login">Sign in</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/profile/shiguanghai">
+              <img class="user-pic" src="https://shiguanghai.top/avatar.png">
+              shiguanghai
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- /顶部导航栏 -->
+
+    <!-- 子路由 -->
+    <nuxt-child/>
+    <!-- /子路由 -->
+
+    <!-- 底部 -->
+    <footer>
+      <div class="container">
+        <a href="/" class="logo-font">conduit</a>
+        <span class="attribution">
+          An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+        </span>
+      </div>
+    </footer>
+    <!-- /底部 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutIndex'
+}
+</script>
+
+<style>
+
+</style>
