15b5cac8af9736c12786b6a5c076bd6f8cb1af95
shiguanghai <1932275668@qq.com>
2021-01-12 22:50:36 +0800

    文章详情代码 及文档更新

--- api/article.js
+++ api/article.js
@@ -41,3 +41,19 @@ export const deleteFavorite = slug => {
     url: `/api/articles/${slug}/favorite`
   })
 }
+
+// 获取文章详情
+export const getArticle = slug => {
+  return request({
+    method: 'GET',
+    url: `/api/articles/${slug}`
+  })
+}
+
+// 获取文章评论
+export const getComments = slug => {
+  return request({
+    method: 'GET',
+    url: `/api/articles/${slug}/comments`
+  })
+}
--- pages/article/index.vue
+++ pages/article/index.vue
@@ -5,26 +5,9 @@
     <div class="banner">
       <div class="container">
 
-        <h1>How to build webapps that scale</h1>
+        <h1>{{ article.title }}</h1>
 
-        <div class="article-meta">
-          <a href=""><img src="http://i.imgur.com/Qr71crq.jpg" /></a>
-          <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-          </div>
-          <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round"></i>
-            &nbsp;
-            Follow Eric Simons <span class="counter">(10)</span>
-          </button>
-          &nbsp;&nbsp;
-          <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart"></i>
-            &nbsp;
-            Favorite Post <span class="counter">(29)</span>
-          </button>
-        </div>
+        <article-meta :article="article" />
 
       </div>
     </div>
@@ -32,86 +15,20 @@
     <div class="container page">
 
       <div class="row article-content">
-        <div class="col-md-12">
-          <p>
-          Web development technologies have evolved at an incredible clip over the past few years.
-          </p>
-          <h2 id="introducing-ionic">Introducing RealWorld.</h2>
-          <p>It's a great solution for learning how other frameworks work.</p>
-        </div>
+        <div class="col-md-12" v-html="article.body"></div>
       </div>
 
       <hr />
 
       <div class="article-actions">
-        <div class="article-meta">
-          <a href="profile.html"><img src="http://i.imgur.com/Qr71crq.jpg" /></a>
-          <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-          </div>
-
-          <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round"></i>
-            &nbsp;
-            Follow Eric Simons <span class="counter">(10)</span>
-          </button>
-          &nbsp;
-          <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart"></i>
-            &nbsp;
-            Favorite Post <span class="counter">(29)</span>
-          </button>
-        </div>
+        <article-meta :article="article" />
       </div>
 
       <div class="row">
 
         <div class="col-xs-12 col-md-8 offset-md-2">
 
-          <form class="card comment-form">
-            <div class="card-block">
-              <textarea class="form-control" placeholder="Write a comment..." rows="3"></textarea>
-            </div>
-            <div class="card-footer">
-              <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-              <button class="btn btn-sm btn-primary">
-              Post Comment
-              </button>
-            </div>
-          </form>
-          
-          <div class="card">
-            <div class="card-block">
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-              <a href="" class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-              </a>
-              &nbsp;
-              <a href="" class="comment-author">Jacob Schmidt</a>
-              <span class="date-posted">Dec 29th</span>
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="card-block">
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-              <a href="" class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-              </a>
-              &nbsp;
-              <a href="" class="comment-author">Jacob Schmidt</a>
-              <span class="date-posted">Dec 29th</span>
-              <span class="mod-options">
-                <i class="ion-edit"></i>
-                <i class="ion-trash-a"></i>
-              </span>
-            </div>
-          </div>
+          <article-comments :article="article" />
           
         </div>
 
@@ -124,8 +41,38 @@
 </template>
 
 <script>
+import { getArticle } from '@/api/article'
+import MarkdownIt from 'markdown-it'
+import ArticleMeta from './components/article-meta.vue'
+import ArticleComments from './components/article-comments.vue'
+
 export default {
-  name: 'ArticleIndex'
+  name: 'ArticleIndex',
+  async asyncData ({ params }) {
+    const { data } = await getArticle(params.slug)
+    const { article } = data
+    const md = new MarkdownIt
+    article.body = md.render(article.body)
+    return {
+      article: article
+    }
+  },
+  components: {
+    ArticleMeta,
+    ArticleComments
+  },
+  head() {
+    return {
+      title: `${this.article.title} - RealWorld`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.article.description
+        }
+      ]
+    }
+  }
 }
 </script>
 
