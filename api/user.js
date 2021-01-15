ece0c2a535c87b4aec9922df46735fb9c98f6972
shiguanghai <1932275668@qq.com>
2021-01-16 00:02:04 +0800

    设置界面修改个人信息及用户登出功能更新

--- api/user.js
+++ api/user.js
@@ -17,3 +17,20 @@ export const register = data => {
     data
   })
 }
+
+// 获取用户信息
+export const getUser = () => {
+  return request({
+    method: 'GET',
+    url: '/api/user'
+  })
+}
+
+// 更新用户信息
+export const updateUser = data => {
+  return request({
+    method: 'PUT',
+    url: '/api/user',
+    data
+  })
+}
--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -10,25 +10,66 @@
           <form>
             <fieldset>
                 <fieldset class="form-group">
-                  <input class="form-control" type="text" placeholder="URL of profile picture">
+                  <input 
+                    class="form-control"
+                    type="text"
+                    placeholder="URL of profile picture"
+                    v-model="user.image"
+                    :disabled="updateDisabled" 
+                  >
                 </fieldset>
                 <fieldset class="form-group">
-                  <input class="form-control form-control-lg" type="text" placeholder="Your Name">
+                  <input 
+                    class="form-control form-control-lg"
+                    type="text"
+                    placeholder="Your Name"
+                    v-model="user.username"
+                    :disabled="updateDisabled" 
+                  >
                 </fieldset>
                 <fieldset class="form-group">
-                  <textarea class="form-control form-control-lg" rows="8" placeholder="Short bio about you"></textarea>
+                  <textarea 
+                    class="form-control form-control-lg"
+                    rows="8"
+                    placeholder="Short bio about you"
+                    v-model="user.bio"
+                    :disabled="updateDisabled" 
+                  ></textarea>
                 </fieldset>
                 <fieldset class="form-group">
-                  <input class="form-control form-control-lg" type="text" placeholder="Email">
+                  <input 
+                    class="form-control form-control-lg"
+                    type="text"
+                    placeholder="Email"
+                    v-model="user.email"
+                    :disabled="updateDisabled" 
+                  >
                 </fieldset>
                 <fieldset class="form-group">
-                  <input class="form-control form-control-lg" type="password" placeholder="Password">
+                  <input 
+                    class="form-control form-control-lg"
+                    type="password"
+                    placeholder="Password"
+                    v-model="user.password"
+                    :disabled="updateDisabled"  
+                  >
                 </fieldset>
-                <button class="btn btn-lg btn-primary pull-xs-right">
+                <button 
+                  class="btn btn-lg btn-primary pull-xs-right"
+                  @click.prevent="updateSettings"
+                  :disabled="updateDisabled"  
+                >
                   Update Settings
                 </button>
             </fieldset>
           </form>
+          <hr>
+          <button
+            class="btn btn-outline-danger"
+            @click="logout"
+          >
+            Or click here to logout.
+          </button>
         </div>
 
       </div>
@@ -38,10 +79,48 @@
 </template>
 
 <script>
+import { getUser, updateUser} from '@/api/user'
+import { mapMutations } from 'vuex'
+
+const Cookie = process.client ? require('js-cookie') : undefined
+
 export default {
   // 在路由匹配组件渲染之前会先执行中间件处理
   middleware: ['authenticated'],
-  name: 'SettingsIndex'
+  name: 'SettingsIndex',
+  data () {
+    return {
+      user: {},
+      updateDisabled: false
+    }
+  },
+  async created () {
+    const { data } = await getUser()
+    this.user = data.user
+  },
+  methods: {
+    ...mapMutations(['setUser']),
+    async updateSettings () {
+      this.updateDisabled = true
+      const { data } = await updateUser({
+        user: this.user
+      })
+      // 保存用户的登录状态
+      // this.$store.commit('setUser', data.user)
+      this.setUser(data.user)
+
+      // 为了防止刷新页面数据丢失，需要将数据持久化
+      Cookie.set('user', data.user)
+      
+      // 跳转到个人主页
+      this.$router.push(`/profile/${data.user.username}`)
+    },
+    logout () {
+      this.setUser(null)
+      Cookie.remove('user')
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
